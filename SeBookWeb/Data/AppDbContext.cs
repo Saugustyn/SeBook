85fbe2fa4307fa18b5d806122a965066280f4996
Sebastian Augustyn <sebastian.augustyn13@gmail.com>
2022-12-19 09:52:43 +0100

    Add AppDbContext Code First

---
+++ SeBookWeb/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SeBookWeb.Models;
+
+namespace SeBookWeb.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}

90ef47ab62e9214faef24979c2cb09a09e4aa8fe
Sebastian Augustyn <sebastian.augustyn13@gmail.com>
2022-12-19 11:23:27 +0100

    Retrieve and Display all Categories

---
+++ SeBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SeBookWeb.Data;
+using SeBookWeb.Models;
+
+namespace SeBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _db;
+        public CategoryController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+    }
+}

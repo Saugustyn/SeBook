3cc5c96f9637b95a3503c8a15c4dcbd3d94e76cc
Sebastian Augustyn <sebastian.augustyn13@gmail.com>
2022-12-19 10:00:49 +0100

    Setup app to use DbContext

--- SeBookWeb/Program.cs
+++ SeBookWeb/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using SeBookWeb.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
 var app = builder.Build();
 

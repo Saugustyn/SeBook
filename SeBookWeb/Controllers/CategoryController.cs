b49210e02e22aabd8b50194fafd36cd047448e61
Sebastian Augustyn <sebastian.augustyn13@gmail.com>
2022-12-20 17:50:17 +0100

    Add Edit Category - GET

--- SeBookWeb/Controllers/CategoryController.cs
+++ SeBookWeb/Controllers/CategoryController.cs
@@ -42,5 +42,42 @@ namespace SeBookWeb.Controllers
             return View(obj);
             
         }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _db.Categories.Find(id);
+            //var categoryFirst = _db.Categories.FirstOrDefault(u=>u.Id == id); // if many records: returns the first record
+            //var categorySingle = _db.Categories.SingleOrDefault(u => u.Id == id); //if many records: throws an exception
+
+            if(category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken] //https://www.devcurry.com/2013/01/what-is-antiforgerytoken-and-why-do-i.html
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("TempCustomValidation", "The DisplayOrder cannot exactly match the Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
     }
 }

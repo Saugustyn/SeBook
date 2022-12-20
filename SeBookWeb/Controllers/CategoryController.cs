ed27b291d23684f1074cb09c59b94bab8419cb70
Sebastian Augustyn <sebastian.augustyn13@gmail.com>
2022-12-20 19:32:10 +0100

    TempData

--- SeBookWeb/Controllers/CategoryController.cs
+++ SeBookWeb/Controllers/CategoryController.cs
@@ -37,6 +37,7 @@ namespace SeBookWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
             return View(obj);
@@ -74,6 +75,7 @@ namespace SeBookWeb.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index");
             }
             return View(obj);
@@ -105,9 +107,8 @@ namespace SeBookWeb.Controllers
         {
             _db.Categories.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index");
-            return View(obj);
-
         }
     }
 }

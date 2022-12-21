8690a72f4e52ff2004f0120328c4756203a05a2b
Sebastian Augustyn <sebastian.augustyn13@gmail.com>
2022-12-21 12:06:45 +0100

    Implement IRepository

--- SeBook.DataAccess/Repository/IRepository/IRepository.cs
+++ SeBook.DataAccess/Repository/IRepository/IRepository.cs
@@ -12,6 +12,7 @@ namespace SeBook.DataAccess.Repository.IRepository
         T GetFirstOrDefault(Expression<Func<T, bool>> filter);
         IEnumerable<T> GetAll();
         void Add(T entity);
+        void Remove(T entity);
 
         void RemoveRange(IEnumerable<T> entity);
     }
---
+++ SeBook.DataAccess/Repository/Repository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SeBook.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeBook.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly AppDbContext _db;
+        internal DbSet<T> dbSet;
+        public Repository(AppDbContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+        }
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> querry = dbSet;
+            return querry.ToList();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> querry = dbSet;
+            querry = querry.Where(filter);
+            return querry.FirstOrDefault(filter);
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            dbSet.RemoveRange(entity);
+        }
+    }
+}

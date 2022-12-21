53243c9e6f4f1f6d6cc8fb9d51d553fe5da3f6a7
Sebastian Augustyn <sebastian.augustyn13@gmail.com>
2022-12-21 12:28:29 +0100

    Implement CategoryRepository

---
+++ SeBook.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SeBook.DataAccess.Repository.IRepository;
+using SeBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeBook.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly AppDbContext _db;
+        public CategoryRepository(AppDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Update(obj);
+        }
+    }
+}

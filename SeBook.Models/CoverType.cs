e03ce56d58ab02b0e79249f4893682ae31a3854b
Sebastian Augustyn <sebastian.augustyn13@gmail.com>
2023-01-01 15:33:43 +0100

    Add CoverType Model

---
+++ SeBook.Models/CoverType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeBook.Models
+{
+    public class CoverType
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Cover Type")]
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+}

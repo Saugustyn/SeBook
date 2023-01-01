6ffe9e78e62aa9d309c3dc40d00d5436e8940487
Sebastian Augustyn <sebastian.augustyn13@gmail.com>
2023-01-01 16:33:28 +0100

    Add CoverType Controllers and Views

---
+++ SeBookWeb/Areas/Admin/Controllers/CoverTypeController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SeBook.DataAccess;
+using SeBook.DataAccess.Repository.IRepository;
+using SeBook.Models;
+
+namespace SeBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CoverTypeController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CoverTypeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<CoverType> objCoverTypeList = _unitOfWork.CoverType.GetAll();
+            return View(objCoverTypeList);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken] //https://www.devcurry.com/2013/01/what-is-antiforgerytoken-and-why-do-i.html
+        public IActionResult Create(CoverType obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.CoverType.Add(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "CoverType created successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            //var category = _db.Categories.Find(id);
+            var coverTypeFirst = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id); // if many records: returns the first record
+            //var categorySingle = _db.Categories.SingleOrDefault(u => u.Id == id); //if many records: throws an exception
+
+            if (coverTypeFirst == null)
+            {
+                return NotFound();
+            }
+            return View(coverTypeFirst);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken] //https://www.devcurry.com/2013/01/what-is-antiforgerytoken-and-why-do-i.html
+        public IActionResult Edit(CoverType obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.CoverType.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "CoverType updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            //var category = _db.Find(id);
+            var coverTypeFirst = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id); // if many records: returns the first record
+            //var categorySingle = _db.Categories.SingleOrDefault(u => u.Id == id); //if many records: throws an exception
+
+            if (coverTypeFirst == null)
+            {
+                return NotFound();
+            }
+            return View(coverTypeFirst);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken] //https://www.devcurry.com/2013/01/what-is-antiforgerytoken-and-why-do-i.html
+        public IActionResult Delete(CoverType obj)
+        {
+            _unitOfWork.CoverType.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "CoverType deleted successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}

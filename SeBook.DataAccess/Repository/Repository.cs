1fd86791f13a07242363c1a524b93fd8e22edcce
Sebastian Augustyn <sebastian.augustyn13@gmail.com>
2023-01-24 12:36:53 +0100

    LOad Shopping Cart List

--- SeBook.DataAccess/Repository/Repository.cs
+++ SeBook.DataAccess/Repository/Repository.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace SeBook.DataAccess.Repository
 {
@@ -25,9 +26,10 @@ namespace SeBook.DataAccess.Repository
             dbSet.Add(entity);
         }
 
-        public IEnumerable<T> GetAll(string? includeProperties = null)
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter, string? includeProperties = null)
         {
             IQueryable<T> querry = dbSet;
+            querry = querry.Where(filter);
             if (includeProperties != null)
             {
                 foreach (var includeProp in includeProperties.Split(new char[] {','}, StringSplitOptions.RemoveEmptyEntries))
---
+++ SeBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SeBook.DataAccess.Repository.IRepository;
+using SeBook.Models.ViewModels;
+using System.Security.Claims;
+
+namespace SeBookWeb.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    [Authorize]
+    public class CartController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartVM ShoppingCartVM { get; set; }
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            ShoppingCartVM = new ShoppingCartVM()
+            {
+                ListCart = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value,
+                includeProperties: "Product")
+            };
+
+            return View(ShoppingCartVM);
+        }
+    }
+}

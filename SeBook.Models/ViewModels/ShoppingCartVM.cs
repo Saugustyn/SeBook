8e3583c891cd0944db0b3741000015f0a350fdd5
Sebastian Augustyn <sebastian.augustyn13@gmail.com>
2023-01-24 15:44:43 +0100

    Summary GET Action Method

--- SeBook.Models/ViewModels/ShoppingCartVM.cs
+++ SeBook.Models/ViewModels/ShoppingCartVM.cs
@@ -9,6 +9,6 @@ namespace SeBook.Models.ViewModels
     public class ShoppingCartVM
     {
         public IEnumerable<ShoppingCart> ListCart { get; set; }
-        public double CartTotal { get; set; }
+        public OrderHeader OrderHeader { get; set; }
     }
 }
